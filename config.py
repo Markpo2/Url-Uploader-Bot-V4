9e9efd467a11ba62fa074ecdb72b0300eed80d3d
Markpo2 <127012533+Markpo2@users.noreply.github.com>
2023-04-03 23:35:02 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", )
+    BOT_TOKEN = os.environ.get("6043283784:AAHLV11M9g3gaDj5dE-Sr5fKhSCd8CT-lOc", )
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", ))
-    API_HASH = os.environ.get("API_HASH",)
+    API_ID = int(os.environ.get("7143337", ))
+    API_HASH = os.environ.get("1afa55a5f3bf7058c843d1b290f79c49",)
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -26,9 +26,9 @@ class Config(object):
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID",))
+    OWNER_ID = int(os.environ.get("1284818583",))
     ADMINS = int(os.environ.get("ADMINS", ))
-    SESSION_NAME = "URLUPLOADERBOT"
+    SESSION_NAME = "uploaderM_bot"
     # database uri (mongodb)
     DATABASE_URL = os.environ.get("DATABASE_URL",)
     DATABASE_NAME = os.environ.get("DATABASE_NAME",)
@@ -37,5 +37,5 @@ class Config(object):
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL",)
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL",)
     #log channel
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
+    LOG_CHANNEL = os.environ.get("-1001465954010")
     
